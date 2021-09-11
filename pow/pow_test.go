3dfdda714725c78d93fda460ce6a4d8e238d694d
hnamzian <namazian87@gmail.com>
2021-09-11 22:22:15 +0300

    Rename TestMineBase to TestMineUntilSuccess

--- pow/pow_test.go
+++ pow/pow_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMineBase(t *testing.T) {
+func TestMineUntilSuccess(t *testing.T) {
 	toBeMined_str := "129df964b701d0b8e72fe7224cc71643cf8e000d122e72f742747708f5e3bb6294c619604e52dcd8f5446da7e9ff7459d1d3cefbcc231dd4c02730a22af9880c"
 	toBeMined_bytes, _ := hex.DecodeString(toBeMined_str)
 
