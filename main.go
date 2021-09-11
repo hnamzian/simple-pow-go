f36734c9a3010ab2c2b8be896c4b993c424aa5a8
hnamzian <namazian87@gmail.com>
2021-09-11 19:41:21 +0300

    Mine input data from cli using pow module

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 
 	"github.com/hnamzian/simple-pow/cli"
+	"github.com/hnamzian/simple-pow/pow"
 )
 
 func main() {
@@ -14,4 +16,22 @@ func main() {
 	}
 
 	fmt.Printf("ToBeMinde Data: %x\n", command.ToBeMined[:])
+
+	// set desired solution (0xcafe)
+	desired_solution_bytes, _ := hex.DecodeString("cafe")
+	// loop until reaching solution
+	max_iters := -1
+	// Mine...
+	nonce, iters, errMine := pow.Mine(command.ToBeMined, desired_solution_bytes, max_iters)
+	if errMine != nil {
+		panic(errMine)
+	}
+	fmt.Printf("Nonce: %x found in %d iterations\n", nonce, iters)
+
+	// verify solution
+	verified, errVerify := pow.VerifyNonce(command.ToBeMined, nonce, desired_solution_bytes)
+	if errVerify != nil {
+		panic(errVerify)
+	}
+	fmt.Printf("Nonce Verified: %v\n", verified)
 }
