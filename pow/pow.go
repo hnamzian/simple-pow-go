1194dae057366d82b4cad67c3984a3e89d3d7a9b
hnamzian <namazian87@gmail.com>
2021-09-11 18:09:04 +0300

    Create pow module to find proof-of-work solution

---
+++ pow/pow.go
@@ -0,0 +1,46 @@
+package pow
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/hnamzian/simple-pow/random"
+
+)
+
+// returns a random nonce which makes hash(nonce + data) to be terminated by a specified 2-byte data
+func Mine(toBeMined []byte) ([]byte, error) {
+	// create value of 2-byte as final solution 
+	desired_solution_bytes, errDecode := hex.DecodeString("cafe")
+	if errDecode != nil {
+		return make([]byte, 0), errDecode 
+	}
+	fmt.Printf("%x\n", desired_solution_bytes)
+
+	// initiate iterative solution with empty 2-byte
+	iter_solution_bytes := make([]byte, 2)
+
+	// initiate nonce of empty 4-byte
+	nonce := make([]byte, 4)
+	
+	// verify if have reached to goal, ie,
+	// last 2 bytes of hashed(nonce + toBeMined) == desired bytes
+	for !bytes.Equal(iter_solution_bytes, desired_solution_bytes) {
+		// generate new 4-byte random data as nonce
+		nonce, _ = random.RandomBytes(4)
+
+		// concat nonce and data to be mined => [nonce,toBeMined]
+		noncedData := append(nonce[:], toBeMined[:]...)
+
+		// calculate hash of appneded data, ie, nonce + toBeMined
+		hashed := sha256.Sum256(noncedData)
+		fmt.Printf("%x\n", hashed[:])
+
+		// exteract last 2 bytes as current solution
+		iter_solution_bytes = hashed[len(hashed)-2:]
+	}
+
+	return nonce, nil
+}
