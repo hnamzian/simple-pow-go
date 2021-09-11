7a0596fbe5b015e4cf1f6643b6fed59737ff3e41
hnamzian <namazian87@gmail.com>
2021-09-11 22:21:35 +0300

    Verify mined signal to be true + Verify mine error to be nil at test

--- pow/pow_test.go
+++ pow/pow_test.go
@@ -14,9 +14,12 @@ func TestMineBase(t *testing.T) {
 	desired_solution_bytes, _ := hex.DecodeString("cafe")
 	max_iters := int64(-1)
 
-	nonce, _, _, _ := Mine(toBeMined_bytes, desired_solution_bytes, max_iters)
+	nonce, _, mined, errMine := Mine(toBeMined_bytes, desired_solution_bytes, max_iters)
+
+	assert.Equal(t, errMine, nil, "Expected not to be failed, Error Message: %s", errMine)
 
 	verified, _ := VerifyNonce(toBeMined_bytes, nonce, desired_solution_bytes)
 
+	assert.Equal(t, mined, true, "mined: expected: true, actual: %v", mined)
 	assert.Equal(t, verified, true, "expected: true, actual: %x", verified)
 }
