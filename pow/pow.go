127ddb936ec66e475dcfadf52dca79b456732b52
hnamzian <namazian87@gmail.com>
2021-09-11 19:00:52 +0300

    Add VerifyNonce function to verify nonce meets pow solution

--- pow/pow.go
+++ pow/pow.go
@@ -7,15 +7,14 @@ import (
 	"fmt"
 
 	"github.com/hnamzian/simple-pow/random"
-
 )
 
 // returns a random nonce which makes hash(nonce + data) to be terminated by a specified 2-byte data
 func Mine(toBeMined []byte) ([]byte, error) {
-	// create value of 2-byte as final solution 
+	// create value of 2-byte as final solution
 	desired_solution_bytes, errDecode := hex.DecodeString("cafe")
 	if errDecode != nil {
-		return make([]byte, 0), errDecode 
+		return make([]byte, 0), errDecode
 	}
 	fmt.Printf("%x\n", desired_solution_bytes)
 
@@ -24,7 +23,7 @@ func Mine(toBeMined []byte) ([]byte, error) {
 
 	// initiate nonce of empty 4-byte
 	nonce := make([]byte, 4)
-	
+
 	// verify if have reached to goal, ie,
 	// last 2 bytes of hashed(nonce + toBeMined) == desired bytes
 	for !bytes.Equal(iter_solution_bytes, desired_solution_bytes) {
@@ -43,4 +42,24 @@ func Mine(toBeMined []byte) ([]byte, error) {
 	}
 
 	return nonce, nil
+}
+
+// returns true if hash of [nonce, toBeMined] will terminate with a specified bytes
+func VerifyNonce(toBeMined []byte, nonce []byte) (bool, error) {
+	// create value of 2-byte as final solution
+	desired_solution_bytes, errDecode := hex.DecodeString("cafe")
+	if errDecode != nil {
+		return false, errDecode
+	}
+
+	// concat nonce and data to be mined => [nonce,toBeMined]
+	noncedData := append(nonce[:], toBeMined[:]...)
+
+	// calculate hash of appneded data, ie, nonce + toBeMined
+	hashed := sha256.Sum256(noncedData)
+
+	// exteract last 2 bytes as current solution
+	solution_bytes := hashed[len(hashed)-2:]
+
+	return bytes.Equal(solution_bytes, desired_solution_bytes), nil
 }
