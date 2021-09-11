72bbccab2b046e577c29ac0041341fd6da010cb4
hnamzian <namazian87@gmail.com>
2021-09-11 17:48:11 +0300

    Read args and create CLI struct ready for PoW mining

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/hnamzian/simple-pow/cli"
+)
+
 func main() {
-	
+	command, errCmd := cli.New(os.Args)
+	if errCmd != nil {
+		panic(errCmd)
+	}
+
+	fmt.Printf("ToBeMinde Data: %x\n", command.ToBeMined[:])
 }
